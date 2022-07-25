352c18b69e983d328e46386882cea4fffc1ed38c
neerajrox408 <neerajagrawal765@gmail.com>
2022-07-25 12:59:47 +0530

    donewith2nd

---
+++ src/main/java/commnadgithubdemo/prcaticecommand.java
@@ -0,0 +1,17 @@
+package commnadgithubdemo;
+
+public class prcaticecommand {
+
+	public static void main(String[] args) {
+String browser="chrome";
+if(browser=="chrome")
+{
+	System.out.println("browser is valid");
+}
+else
+{
+	System.out.println("browser not valid");
+}
+	}
+
+}
